f1969ca7444a1a29dbca68f6cbf1e00b85c589bb
lingarajuv2005 <lingarajuv2005@gmail.com>
2026-01-13 09:57:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
-// Firebase setup (DO NOT DELETE)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAnalytics } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCjJWYGKvqSITZaunoGqSCL-1DHhXD4TX0",
@@ -8,8 +10,10 @@ const firebaseConfig = {
   projectId: "my-time-table-2fd74",
   storageBucket: "my-time-table-2fd74.firebasestorage.app",
   messagingSenderId: "1093466719154",
-  appId: "1:1093466719154:web:f0297e6aa292df6c4adaab"
+  appId: "1:1093466719154:web:f0297e6aa292df6c4adaab",
+  measurementId: "G-Z1WWELFN4T"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// ✅ EXPORT APP
+export const app = initializeApp(firebaseConfig);
+getAnalytics(app);
