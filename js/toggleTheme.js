8cb32cce0dd551a5475ed4c6c79a01ad54abba7f
lingarajuv2005 <lingarajuv2005@gmail.com>
2026-01-13 10:10:55 +0530

    Create toggleTheme.js

---
+++ js/toggleTheme.js
@@ -0,0 +1,16 @@
+window.toggleDark = function () {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
+
+// load saved theme
+window.addEventListener("load", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+});
